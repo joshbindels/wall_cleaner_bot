109e2e43397994e383ee1d72fab399b09086a35f
joshbindels <josh.bindels@gmail.com>
2020-05-25 21:21:14 +0200

    Instead of interfacing with the rosbridge through python, use the javascript library

--- src/ActionClient.py
+++ src/ActionClient.py
@@ -1,26 +1,67 @@
+from __future__ import print_function
+import time
+
 from flask import Flask
 from flask import render_template
-from RosWrapper import RosWrapper
 import roslibpy
 
 app = Flask(__name__)
+client = None
+talker = None
 
-client = roslibpy.Ros(host="192.168.1.104", port=9090)
 
 
 @app.route("/")
 def index(name=None):
+    return render_template("thing.html")
+
+@app.route("/one")
+def one():
+    if client.is_connected:
+        talker.publish(roslibpy.Message({"data": "One"}))
+    else:
+        print("Client is not connected")
     return render_template("index.html")
 
-@app.route("/hello_world")
-def hello_world():
-    action_client = roslibpy.actionlib.ActionClient(client)
-    goal = roslibpy.actionlib.Goal(action_client, roslibpy.Message({"order": 8}))
-    goal.send()
-    result = goal.wait(10)
-    action_client.dispose()
-    return render_template("index.html", {"data": result})
+@app.route("/two")
+def two():
+    if client.is_connected:
+        talker.publish(roslibpy.Message({"data": "Two"}))
+    else:
+        print("Client is not connected")
+    return render_template("index.html")
 
 if __name__ == "__main__":
+    """
+    client = roslibpy.Ros(host="192.168.1.104", port=9090)
     client.run()
-    app.run(debug=True)
+    talker = roslibpy.Topic(client, "/chatter", "std_msgs/String", throttle_rate=10)
+    """
+    app.run(host="0.0.0.0", debug=True)
+    """
+    talker.unadvertise()
+    client.terminate()
+    """
+    """
+    client = roslibpy.Ros(host="192.168.1.104", port=9090)
+    client.on_ready(lambda: print("Client is ready"))
+    client.run()
+
+    talker = roslibpy.Topic(client, "/chatter", "std_msgs/String")
+    talker.advertise()
+
+    while not talker.is_advertised:
+        time.sleep(0.5)
+
+    for i in range(10):
+        if client.is_connected:
+            print("Publishing message")
+            talker.publish(roslibpy.Message({"data": "Hello"}))
+        else:
+            print("Client is not connected")
+        time.sleep(1)
+
+    talker.unadvertise()
+    client.close()
+    client.terminate()
+    """
